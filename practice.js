852323365fed313c77af37bf97ef86ee2b9c00aa
Harshit nehra <nehraharshit01@gmail.com>
2025-04-18 19:32:06 +0530

    learning

--- practice.js
+++ practice.js
@@ -1,1 +1,33 @@
-console.log();
+// function debounce(func, delay) {
+//     let timer;
+//     return function (...args) {
+//         clearTimeout(timer);
+//         timer = setTimeout(() => func(...args), delay);
+//     };
+// }
+
+// // Example Usage
+// const fetchData = (query) => {
+//     console.log(`Fetching data for ${query}`);
+// };
+
+// const debouncedSearch = debounce(fetchData, 5000);
+
+// // Simulating rapid user typing
+// debouncedSearch("J");
+// debouncedSearch("Ja");
+// debouncedSearch("Jav");
+// debouncedSearch("Java");  // ✅ Only "Fetching data for Java" will be logged after 500ms
+
+function helloPattern() {
+  let rowPattern = [1, 0, 1, 0, 1, 0, 1];
+  
+  for (let i = 0; rowPattern.length > 0; i++) {
+    let spaces = ' '.repeat(i * 2);
+    console.log(spaces + rowPattern.join(' '));
+    rowPattern.shift();
+    rowPattern.pop();
+  }
+}
+
+helloPattern();
