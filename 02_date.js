cc96b23b503e29628d9c67976c48aa68a1d88890
Harshitnehra <158832953+Harshitnehra@users.noreply.github.com>
2026-01-24 12:27:58 +0530

    Update 02_date.js

--- 02_date.js
+++ 02_date.js
@@ -1,6 +1,4 @@
 let newdate = new Date()
-// console.log(newdate.toString());
-// console.log(newdate.toISOString());
 console.log(typeof newdate);
 
 console.log(newdate.toLocaleString({hour12 : ""}));
