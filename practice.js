a7a9b9d19b1b276cb4365648c4cf2c4f091b1f53
Harshit nehra <nehraharshit01@gmail.com>
2026-02-06 16:06:01 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,4 +1,46 @@
-function show() {
-  console.log(this);
+// // console.log(a)
+// // var a = 10
+
+// // console.log(s)
+// // let s = 10
+
+// // function hosting
+// // sayHello();
+
+// // function sayHello() {
+// //   console.log("Hello");
+// // }
+
+// // sayhi()
+// //  var sayhi = function(){
+// //   console.log("sayhi");
+// // }
+
+// // sayhi()
+
+// const a = 1;
+
+// function outer() {
+//   console.log(a);
+//   // var a = 2;
+// }
+
+// outer();
+// const numbers = [5, 10, 15];
+
+// console.log(...numbers);
+function sum(a, ...numbers) {
+  console.log(a , numbers);
 }
-show(); // In browser -> Window
+
+sum(1, 2, 3, 4);
+
+
+const arr = [1, 2, 3];
+
+const result = arr.reduce((acc, curr) => {
+  acc.push(curr * 2);
+  return acc;
+}, []);
+
+console.log(result);
